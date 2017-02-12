9fd997125255850cf8e987690cbc495098e06343
Lukas Winkler <l.winkler23@mailbox.org>
2017-02-12 21:41:51 +0100

    add add.py

---
+++ add.py
@@ -0,0 +1,29 @@
+import sys
+import yaml
+from urllib.parse import urlparse
+
+with open("sites.yml", 'r') as stream:
+    sites = yaml.load(stream)
+
+try:
+    name = sys.argv[1]
+    feedurl = input("URL: ")
+    parsed = urlparse(feedurl)
+    if not (parsed.scheme and parsed.netloc and parsed.path):
+        print("invalid URL")
+        exit()
+    tags = []
+    while True:
+        tag = input("Tag: ")
+        if tag == "":
+            break
+        tags.append(tag)
+
+    sites[name] = {"url": feedurl, "tags": tags}
+
+except Exception as e:
+    print("invalid input")
+    print(e)
+
+with open('sites.yml', 'w') as outfile:
+    yaml.dump(sites, outfile, default_flow_style=False)
